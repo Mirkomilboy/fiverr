adce2505273d94213d1634a0823d0a2a378443b4
Mirkomilboy <msadirov1997@gmail.com>
2024-05-26 01:09:11 +0400

    fix card

--- components/common/VideoComponent.tsx
+++ components/common/VideoComponent.tsx
@@ -17,6 +17,8 @@ const VideoComponent = ({ like, videoLink, imgLink }: cardI) => {
     }
   }, [isVideoPlaying])
 
+  let randomId = 'heart' + Math.random() * 10
+
   return (
     <div
       onMouseEnter={() => setIsVideoPlaying(true)}
@@ -34,9 +36,9 @@ const VideoComponent = ({ like, videoLink, imgLink }: cardI) => {
       {imgLink && (
         <Image className='absolute size-full object-cover' fill src={imgLink} alt='' />
       )}
-      <label className='absolute z-20 top-4 right-4' htmlFor="#heart">
-        <input className='sr-only' type="checkbox" id='heart' />
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="size-8 !fill-black/40 cursor-pointer">
+      <label className='absolute z-20 top-4 right-4 cursor-pointer' htmlFor={randomId.toString()}>
+        <input className='sr-only peer [&:checked+svg]:!fill-red-500' type="checkbox" id={randomId.toString()} />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="size-8 pointer-events-none fill-black/40">
           <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
         </svg>
       </label>
