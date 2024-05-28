eec62e78a082eeff34a36273dd65cb72494a9535
AbdushukurRasulov <abdushukurrasulov1994@gmail.com>
2024-05-28 06:43:28 +0500

    icon rotated

--- components/DropdownSelect.tsx
+++ components/DropdownSelect.tsx
@@ -18,7 +18,11 @@ const DropdownSelect = ({ id, title, data, handleOpenDropdown, open }: any) => {
           id == open ? "bg-brand-slate-100" : ""
         } flex items-center gap-3 text-brand-black-100 font-medium h-12 min-w-14 border border-brand-black-200 hover:border-brand-black rounded-lg transition-colors duration-200 py-3 px-4`}>
         <span>{title}</span>
-        <svg className="size-3" viewBox="0 0 11 7" xmlns="http://www.w3.org/2000/svg" fill="currentFill">
+        <svg
+          className={`size-3 ${id == open ? "rotate-180" : ""} transition-transform duration-200`}
+          viewBox="0 0 11 7"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentFill">
           <path d="M5.464 6.389.839 1.769a.38.38 0 0 1 0-.535l.619-.623a.373.373 0 0 1 .531 0l3.74 3.73L9.47.61a.373.373 0 0 1 .531 0l.619.623a.38.38 0 0 1 0 .535l-4.624 4.62a.373.373 0 0 1-.531 0Z"></path>
         </svg>
       </button>
